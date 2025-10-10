43362ff36e1171f513f3ebe2a47c647836f933d7
Ameen <devameen45@gmail.com>
2025-10-10 09:25:44 -0700

    update vite.config.js

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  server: {
+    host: true, // 👈 allows external access when testing on network/server
+    allowedHosts: [
+      "naridevelopment.org",
+      "www.naridevelopment.org", // 👈 include www for safety
+      "localhost",
+    ],
+  },
+  plugins: [tailwindcss()],
+});
